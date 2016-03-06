cb227ce71e1e4838f92d246dff3c35a07ee38c2e
christianpetri <christianpetri@hotmail.com>
2016-03-06 18:00:02 +0100

    Working added site jump

--- java/java.js
+++ java/java.js
@@ -1,13 +1,16 @@
 $(document).ready(function() {
 	
 	"use strict";
+	
+	
+	/**Nachrichten Fromular Start*/
 	$(".button").click(function(){
   		var fname = $('input[name=fname]').val();
   		var lname = $('input[name=lname]').val();
   		var nachricht = $('textarea[name=nachricht]').val();
 		$(".list").empty();
   		$(".list").append('<div class="item">'+"<br/>" +"Nachricht von "+ fname +" "+ lname + "<br/>" + " Nachricht: " + nachricht + "<br/>"+ '</div>');
-		$(".list").append('<div>' + '<input class="button2" type="submit" value="&Auml;ndern" />' + '</div>');
+		$(".list").append('<div>' + '<input class="button2" type="submit" value="Nachricht &Auml;ndern" />' + '</div>');
 		$(".nachricht").hide();
 		
 			$(".button2").click(function(){
@@ -15,6 +18,7 @@ $(document).ready(function() {
 				$(".nachricht").show();
 	
 			});
+		/**Nachrichten Fromular Ende*/
 		
 	});
 	
