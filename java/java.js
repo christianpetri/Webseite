158f6cbc874622c492e62ac275f31cfc721ef321
christianpetri <christianpetri@users.noreply.github.com>
2016-03-06 10:28:36 +0100

    Added files via upload

---
+++ java/java.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+
+  var fname = $('input[name=dataNachricht]').val();
+  var lname = $('input[name=dataNachricht]').val();
+  var nachricht = $('input[name=dataNachricht]').val();
+  $(".item").append('<div class="item">' + fname + '</div>');
+
+});
