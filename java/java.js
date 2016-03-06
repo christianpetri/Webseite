64dbe0e7d2c90f5cccd56742cdc415ebe1e8abf8
christianpetri <christianpetri@hotmail.com>
2016-03-06 17:03:37 +0100

    Formular + fixTop + fixBottom

--- java/java.js
+++ java/java.js
@@ -6,6 +6,18 @@ $(document).ready(function() {
   		var lname = $('input[name=lname]').val();
   		var nachricht = $('textarea[name=nachricht]').val();
 		$(".list").empty();
-  		$(".list").append('<div class="item">'+"<br/>" + fname +" "+ lname + "<br/>" + " Nachricht: " + nachricht + "<br/>"+ '</div>');
-});
+  		$(".list").append('<div class="item">'+"<br/>" +"Nachricht von "+ fname +" "+ lname + "<br/>" + " Nachricht: " + nachricht + "<br/>"+ '</div>');
+		$(".list").append('<div>' + '<input class="button2" type="submit" value="&Auml;ndern" />' + '</div>');
+		$(".nachricht").hide();
+		
+			$(".button2").click(function(){
+		
+				$(".nachricht").show();
+	
+			});
+		
+	});
+	
+	
+
 });
