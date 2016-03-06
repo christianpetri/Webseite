7554beb0aed9c4adf3f088615619b567c4f14405
christianpetri <christianpetri@hotmail.com>
2016-03-06 14:33:09 +0100

    Append Nachicht working

--- java/java.js
+++ java/java.js
@@ -5,6 +5,7 @@ $(document).ready(function() {
   		var fname = $('input[name=fname]').val();
   		var lname = $('input[name=lname]').val();
   		var nachricht = $('textarea[name=nachricht]').val();
-  		$(".list").append('<div class="item">'+"<br/>" + fname +" "+ lname + "<br/>" + " Nachricht: " + nachricht + "<br/>"+'</div>');
+		$(".list").empty();
+  		$(".list").append('<div class="item">'+"<br/>" + fname +" "+ lname + "<br/>" + " Nachricht: " + nachricht + "<br/>"+ '</div>');
 });
 });
