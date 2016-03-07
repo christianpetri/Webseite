e9868b25345ad54615e7ab1465a10d32e09a0187
christianpetri <christianpetri@hotmail.com>
2016-03-07 16:02:24 +0100

    Code cleaned up

--- js/jump.js
+++ js/jump.js
@@ -1,53 +1,68 @@
-var score = 0, highScore=0, highScoreText=0;
-var cactus = 0,x = 0,catusHeight=0,cactusupdown=1;
-var dino = 0,ydino=0;
+var score = 0, highScore=0;
+var cactus = 0,x = 0,dinoHeight=0,dinoUpDown=1;
+var cactusPosition=0;
 var space = false;
 var spacebar=0;
-var t=0;
-
+var t=0; 
+var start=0;
 
 $(document).ready(function(){
 	"use strict";
 	setInterval(function(){
-			$(".highScoreText").empty();
-			if(score>highScore){highScore=score;}
-			$(".highScoreText").append("Score: "+ score);
-			$(".highScoreText").append(" Highscore: "+ highScore);  
-			$(".highScoreText").append("<br/>"+"catusHeight: "+ catusHeight);
-			if(true){score+=1;}else{score=0;}
-			x=$(".block").position(); 
-			x.top=Math.floor(x.top);
-			$(".highScoreText").append("<br/>"+" top: "+ x.top +" left: "+x.left);
-			cactus=$(".blockcactus").position(); 
-			 
-			$(".highScoreText").append("<br/>"+"catus.left: "+ cactus.left);
-			
-			ydino+=1; 
-			if(ydino>500){ydino=0;}
-			$(".blockcactus").css("right", ydino);  
-			
-			if(spacebar===1){
-				t+=1;
-				if(catusHeight===0||catusHeight<0){cactusupdown=1;}
-				if(catusHeight===39||catusHeight>40){cactusupdown=-1;}
-				catusHeight+=cactusupdown;
+		if(start){ 	
+			 	$(".highScoreText").empty();
+				if(score>highScore){highScore=Math.floor(score);}
+				$(".highScoreText").append("Score: "+ Math.floor(score)+"    ");
+				$(".highScoreText").append("Highscore: "+ highScore);  
+				//*$(".highScoreText").append("<br/>"+"dinoHeight: "+ dinoHeight);*/
+				score+=0.01;
 				
-				if(t>77 ){t=0;spacebar=0;catusHeight=0;} 
-				$(".block").css("bottom", catusHeight);
-			}
-			
+				x=$(".block").position(); 
+				x.top=Math.floor(x.top);
+				//*$(".highScoreText").append("<br/>"+" top: "+ x.top +" left: "+x.left);*/
+				cactus=$(".blockcactus").position(); 
+				 
+				//*$(".highScoreText").append("<br/>"+"catus.left: "+ cactus.left);*/
+				
+				cactusPosition+=1; 
+				if(cactusPosition>520){cactusPosition=0;}
+				$(".blockcactus").css("right", cactusPosition);  
+				
+				if(spacebar===1){
+					t+=1;
+					if(dinoHeight===0||dinoHeight<0){dinoUpDown=1;}
+					if(dinoHeight===39||dinoHeight>40){dinoUpDown=-1;}
+					dinoHeight+=dinoUpDown;
+					
+					if(t>77 ){t=0;spacebar=0;dinoHeight=0;} 
+					$(".block").css("bottom", dinoHeight);
+					 
+				}
+				if(dinoHeight<10){ 
+						  if(cactus.left<10&&cactus.left>0){start=false; 
+						  	
+						  	$(".highScoreText").empty();
+							$(".highScoreText").append("Score: "+ Math.floor(score)+"    ");
+							$(".highScoreText").append("Highscore: "+ highScore); 
+							cactusPosition+=30;
+							score=0; 
+						  }
+				}
 			 
+		} 	 
 	}, 10 );
-
- 
+	
+   
 });
 $(document).keydown(function(key) {
 			
 	"use strict";	
 	if (key.keyCode === 32) {
 		space = true;
+		start=true;
 		spacebar=1; 
 		$(".block").css("background-color", "yellow"); 
+		
 													
 	}
 }).keyup(function(key){
@@ -56,7 +71,7 @@ $(document).keydown(function(key) {
 	if (key.keyCode === 32) {
 		 	space = false;
 			$(".block").css("background-color", "blue");
-		  
+		  start=true;
 	}
 			
 });
