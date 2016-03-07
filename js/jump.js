2dd1c126507807199cbddf0686aff13310c0553b
christianpetri <christianpetri@hotmail.com>
2016-03-07 14:24:38 +0100

    Jump working, now lets set up the score if then thingy

--- js/jump.js
+++ js/jump.js
@@ -2,7 +2,8 @@ var score = 0, highScore=0, highScoreText=0;
 var cactus = 0,x = 0,catusHeight=0,cactusupdown=1;
 var dino = 0,ydino=0;
 var space = false;
-
+var spacebar=0;
+var t=0;
 
 
 $(document).ready(function(){
@@ -13,37 +14,31 @@ $(document).ready(function(){
 			$(".highScoreText").append("Score: "+ score);
 			$(".highScoreText").append(" Highscore: "+ highScore);  
 			$(".highScoreText").append("<br/>"+"catusHeight: "+ catusHeight);
+			if(true){score+=1;}else{score=0;}
 			x=$(".block").position(); 
 			x.top=Math.floor(x.top);
 			$(".highScoreText").append("<br/>"+" top: "+ x.top +" left: "+x.left);
-			cactus=$(".block").position(); 
+			cactus=$(".blockcactus").position(); 
 			 
-			$(".highScoreText").append("<br/>"+"catus.top: "+ cactus.top);
-			if(catusHeight===39||catusHeight>40){cactusupdown=-1;}
-			if(catusHeight===0||catusHeight<0){cactusupdown=1;}
-			catusHeight+=cactusupdown;
-			ydino+=1;
+			$(".highScoreText").append("<br/>"+"catus.left: "+ cactus.left);
+			
+			ydino+=1; 
 			if(ydino>500){ydino=0;}
-			$(".block").css("bottom", catusHeight);
-			$(".blockcactus").css("right", ydino);
-	}, 20);
-		
-
+			$(".blockcactus").css("right", ydino);  
+			
+			if(spacebar===1){
+				t+=1;
+				if(catusHeight===0||catusHeight<0){cactusupdown=1;}
+				if(catusHeight===39||catusHeight>40){cactusupdown=-1;}
+				catusHeight+=cactusupdown;
+				
+				if(t>77 ){t=0;spacebar=0;catusHeight=0;} 
+				$(".block").css("bottom", catusHeight);
+			}
+			
 			 
-		
-		
-		
-		
-		
-	
-		
-	
-		x=$(".block").position(); 
-		/**x.top=Math.floor(x.top);*/
-		$(".highScoreText").append("<br/>"+" top: "+ x.top +" left: "+x.left);
-		$(".highScoreText").append("<br/>"+"catusHeight: "+ catusHeight);
-		
-		
+	}, 10 );
+
  
 });
 $(document).keydown(function(key) {
@@ -51,24 +46,16 @@ $(document).keydown(function(key) {
 	"use strict";	
 	if (key.keyCode === 32) {
 		space = true;
-		/**$( ".block" ).animate({ "top": "-=30px" }, "slow" );*/
+		spacebar=1; 
 		$(".block").css("background-color", "yellow"); 
-		catusHeight+=10;
-		$(".highScoreText").empty();
-		$(".highScoreText").append("Score: "+ score);
-		if(space===true){
-			score+=1;
-			
-		}
-										
+													
 	}
 }).keyup(function(key){
 	"use strict";
 
 	if (key.keyCode === 32) {
-		 space = false;
-		 /**$( ".block" ).animate({ "top": "+=30px" }, "slow" );*/
-		 $(".block").css("background-color", "blue");
+		 	space = false;
+			$(".block").css("background-color", "blue");
 		  
 	}
 			
