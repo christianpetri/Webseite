e22d15d0cf6ef665d376b2465d855dd543be951f
christianpetri <christianpetri@hotmail.com>
2016-03-07 11:11:16 +0100

    working

--- java/jump.js
+++ java/jump.js
@@ -1,42 +1,70 @@
 var score = 0, highScore=0, highScoreText=0;
-var cactus = 0,x = 0;
+var cactus = 0,x = 0,catusHeight=0;
 var dino = 0,ydino=0;
 var space = false;
 
 $(document).ready(function(){
 	"use strict";
-if(highScore===0){highScoreText="";}else{highScoreText="Highscore: "+ highScore;}
-if(score>=6){
-		highScore=score;
-		highScore=true;
-}
-$(".highScoreText").append("Score: "+ score);
+	if(highScore===0){highScoreText="";}else{highScoreText="Highscore: "+ highScore;}
+	if(score>=6){
+			highScore=score;
+			}
+	x=$(".block").position(); 
+	/**x.top=Math.floor(x.top);*/
+	$(".highScoreText").append("Score: "+ score);
+	$(".highScoreText").append("<br/>"+" top: "+ x.top +" left: "+x.left);
+	$(".highScoreText").append("<br/>"+"catusHeight: "+ catusHeight);
+	//** $(document).keyup(function(evt) {
+	//**    if (evt.keyCode == 32) {
+	//**      space = false;
+	//**    }
+	 //** }).keydown(function(evt) {
+	 //**   if (evt.keyCode == 32) {
+	//**      space = true;
+	//**    }
+	//**  });
+	//**}
+	if(space===false){catusHeight=0;}
+	if(score>highScore){highScore=score;}
 
-$(document).keydown(function(key) {
+	$(".highScoreText").empty();
+	$(".highScoreText").append("Score: "+ score);
+	$(".highScoreText").append(" Highscore: "+ highScore);
+
+	x=$(".block").position(); 
+	/**x.top=Math.floor(x.top);*/
+	$(".highScoreText").append("<br/>"+" top: "+ x.top +" left: "+x.left);
+	$(".highScoreText").append("<br/>"+"catusHeight: "+ catusHeight);
+	
 		
-			if (key.keyCode === 32) {
-				 space = true;
-				 $( ".block" ).animate({ "top": "-=30px" }, "slow" );
-			}
 						
-			if(space===true){score+=1;
-			}
-$(".highScoreText").empty();
-$(".highScoreText").append("Score: "+ score);
-
-						
+		
+ 
 });
-$(document).keyup(function(key) {	
+$(document).keydown(function(key) {
+			
+	"use strict";	
+	if (key.keyCode === 32) {
+		space = true;
+		/**$( ".block" ).animate({ "top": "-=30px" }, "slow" );*/
+		$(".block").css("background-color", "yellow"); 
+		catusHeight+=10;
+		$(".highScoreText").empty();
+		$(".highScoreText").append("Score: "+ score);
+		if(space===true){
+			score+=1;
+			catusHeight+=10;
+			$(".block").css("bottom", catusHeight);
+		}								
+	}
+}).keyup(function(key){
+	"use strict";
 
-			var space = false;
-			if (key.keyCode === 32) {
-				 space = false;
-				 $( ".block" ).animate({ "top": "+=30px" }, "slow" );
-			}
+	if (key.keyCode === 32) {
+		 space = false;
+		 /**$( ".block" ).animate({ "top": "+=30px" }, "slow" );*/
+		 $(".block").css("background-color", "blue");
+		  catusHeight=0;
+	}
 			
 });
-			
-
-});
-	
-	
