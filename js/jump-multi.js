679a31fea450769b98f554fc166c5aba11b92846
christianpetri <christianpetri@hotmail.com>
2016-03-08 16:54:40 +0100

    Let get that explode thingy working

--- js/jump-multi.js
+++ js/jump-multi.js
@@ -75,11 +75,65 @@ function displayScoreText(){
 	
 }
 
-function endOfGame(stopGame){
+
+function dinoJump(){
 	"use strict";
+	if(spacebar===1){
+		t+=1;
+		if(dinoHeight===0||dinoHeight<0){dinoUpDown=1;}
+		if(dinoHeight===39||dinoHeight>40){dinoUpDown=-1;}
+		dinoHeight+=dinoUpDown;
+		
+		if(t>77 ){t=0;spacebar=0;dinoHeight=0;} 
+		
+		$(".block").css("bottom", dinoHeight);
+										 
+	}
+}
+function dinoKill(){
+	"use strict";
+		if(dinoHeight<10){ 
+					if(cactus0.left<25&&cactus0.left>15){stopGame=true;}
+					if(cactus1.left<25&&cactus1.left>15){stopGame=true;}
+					if(cactus2.left<25&&cactus2.left>15){stopGame=true;}			
+		}
+		if(stopGame){  
+					endOfGame(stopGame);	
+					stopGame=false;
+		}
+}
+function getPositon(){
+	"use strict";
+		/**get the position of the block "top,left"*/
+				x=$(".block").position(); 
+				x.top=Math.floor(x.top);
+				
+				/**get the position of the cactus0"x,y"*/
+				cactus0=$(".blockcactus0").position(); 
+				cactus1=$(".blockcactus1").position();
+				cactus2=$(".blockcactus2").position();
+}
+function moveAndResetPosition(){
+	"use strict";
+		/**move catus from right to left count up*/ 
+				if(Math.abs(cactusPosition0-cactusPosition2 )>=0 )	{cactusPosition0 	+=speed; }
+				if(Math.abs(cactusPosition0-cactusPosition1 )>170)	{cactusPosition1	+=speed; }
+				if(Math.abs(cactusPosition1-cactusPosition2)>170)	{cactusPosition2	+=speed; }
+		/**if cactus0is of the far left, reset is to right position*/
+				if((cactusPosition0) > 520){cactusPosition0 = -1*(Math.floor((Math.random()*8+1))*10+20);}
+				if((cactusPosition1) > 520){cactusPosition1 = -1*(Math.floor((Math.random()*8+1))*10+20);}
+				if((cactusPosition2) > 520){cactusPosition2 = -1*(Math.floor((Math.random()*8+1))*10+20);}
+		/**Place catus and create the movement*/
+				$(".blockcactus0").css("right", cactusPosition0	);  
+				$(".blockcactus1").css("right", cactusPosition1	);  
+				$(".blockcactus2").css("right", cactusPosition2	);
+}
+function endOfGame(stopGame){
+	"use strict"; 
 	console.log("Stop "+stopGame);
 	console.log("Start "+start);
 	$(".block").css("background-color", "tomato");
+	$(".block").effect('explode');
 	$(".highScoreText").empty();
 	$(".scoreText").empty();
 	$(".scoreText").append("Score: "+ Math.floor(score) );
@@ -95,52 +149,4 @@ function endOfGame(stopGame){
 	console.log("Stop Nachher "+stopGame);
 	console.log("Start Nachher "+start);
 
-}
-function dinoJump(){
-	if(spacebar===1){
-		t+=1;
-		if(dinoHeight===0||dinoHeight<0){dinoUpDown=1;}
-		if(dinoHeight===39||dinoHeight>40){dinoUpDown=-1;}
-		dinoHeight+=dinoUpDown;
-		
-		if(t>77 ){t=0;spacebar=0;dinoHeight=0;} 
-		
-		$(".block").css("bottom", dinoHeight);
-										 
-	}
-}
-function dinoKill(){
-		if(dinoHeight<10){ 
-					if(cactus0.left<25&&cactus0.left>15){stopGame=true;}
-					if(cactus1.left<25&&cactus1.left>15){stopGame=true;}
-					if(cactus2.left<25&&cactus2.left>15){stopGame=true;}			
-		}
-		if(stopGame){  
-					endOfGame(stopGame);	
-					stopGame=false;
-		}
-}
-function getPositon(){
-		/**get the position of the block "top,left"*/
-				x=$(".block").position(); 
-				x.top=Math.floor(x.top);
-				
-				/**get the position of the cactus0"x,y"*/
-				cactus0=$(".blockcactus0").position(); 
-				cactus1=$(".blockcactus1").position();
-				cactus2=$(".blockcactus2").position();
-}
-function moveAndResetPosition(){
-		/**move catus from right to left count up*/ 
-				if(Math.abs(cactusPosition0-cactusPosition2 )>=0 )	{cactusPosition0 	+=speed; }
-				if(Math.abs(cactusPosition0-cactusPosition1 )>170)	{cactusPosition1	+=speed; }
-				if(Math.abs(cactusPosition1-cactusPosition2)>170)	{cactusPosition2	+=speed; }
-		/**if cactus0is of the far left, reset is to right position*/
-				if((cactusPosition0) > 520){cactusPosition0  = -1*(Math.floor((Math.random()*8+1))*10+20);}
-				if((cactusPosition1) > 520){cactusPosition1 = -1*(Math.floor((Math.random()*8+1))*10+20);}
-				if((cactusPosition2) > 520){cactusPosition2 = -1*(Math.floor((Math.random()*8+1))*10+20);}
-		/**Place catus and create the movement*/
-				$(".blockcactus0").css("right", cactusPosition0	);  
-				$(".blockcactus1").css("right", cactusPosition1	);  
-				$(".blockcactus2").css("right", cactusPosition2	);
 }
