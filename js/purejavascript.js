6469765657056635eaff3125acf8b1112481a584
christianpetri <christianpetri@hotmail.com>
2016-03-11 16:40:41 +0100

    moving ball

--- js/purejavascript.js
+++ js/purejavascript.js
@@ -1,41 +1,49 @@
 // JavaScript Document
 
+function game(){
+	"use strict";
+var ball={x:0,y:0};
+
+ 
+
+setInterval(function(){
+	
+		
+	ball.x+=1;
+	if(ball.x>100){ball.x=0;}
+	console.log(ball.x);
+	
+	document.getElementById("ball").style.top=ball.x+"px";
+	},300);
+
+ 
+}
+
 
 function move(event) {
 	"use strict";
-    var k = event.keyCode,
+    var key = event.keyCode,
         chrId = document.getElementById('moveupdown'),
         chr = {
             updown: function () {
                 var y = parseInt(getComputedStyle(chrId).top);
-                if (k === 38) {
-                   if(y>0){
-					y-=10;
+					if (key === 38) {
+					   if(y>0){
+							y-=10;
+						}
+					} else if (key === 40) {
+						if(y<=210){
+						y+=10;
+						}
+						 
 					}
-                } else if (k === 40) {
-                    if(y<=210){
-					y+=10;
+					
+					return y;
 					}
-					console.log(y);
-                }
-                
-                return y;
-            },
-
-            leftright: function () {
-                var x = parseInt(getComputedStyle(chrId).left);
-                if (k === 37) {
-                    --x;
-                } else if (k === 39) {
-                    ++x;
-                }
-                
-                return x;
-            }
         };
 
     chrId.style.top = (chr.updown()) + "px";
-    chrId.style.left = (chr.leftright()) + "px";
+   
 }
 
 document.addEventListener('keydown', move);
