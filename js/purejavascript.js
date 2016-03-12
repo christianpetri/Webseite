f5173dab565f7eeffc52ad1604d422e09d0d5986
christianpetri <christianpetri@hotmail.com>
2016-03-12 21:52:30 +0100

    boundary conditions set

--- js/purejavascript.js
+++ js/purejavascript.js
@@ -2,24 +2,34 @@
 
 function game(){
 	"use strict";
-var ball={x:150,y:150};
+var ball={x:150,y:350};
+var dir={x:1,y:1};
 
  
 
 setInterval(function(){
 	
-		
-	ball.x+=10;
-	if(ball.x>=300){ball.x=150;}
-	console.log(ball.x);
+	if(ball.x<=0){dir.x=1;}
+	if(ball.x>=290){dir.x=-1;}
+	if(ball.y<=0){dir.y=1;}
+	if(ball.y>=690){dir.y=-1;}	
+	ball.x+=10*dir.x;
+	ball.y+=10*dir.y;
+	//var direction=Math.floor(Math.random()*9+1);
+	//console.log("ball.x= "+ball.x);
+	//console.log("top= "+document.getElementById("ball").style.top);
+	//console.log("left= "+document.getElementById("ball").style.left);
 	
 	document.getElementById("ball").style.top=ball.x+"px";
-	document.getElementById("ball").style.left=ball.x+"px";
+	document.getElementById("ball").style.left=ball.y+"px";
+	
+	move();
+	console.log((chr.updown()) + "px");
 	},300);
 
  
-}
 
+}
 
 function move(event) {
 	"use strict";
@@ -33,7 +43,7 @@ function move(event) {
 							y-=10;
 						}
 					} else if (key === 40) {
-						if(y<=210){
+						if(y<=290){
 						y+=10;
 						}
 						 
@@ -44,6 +54,7 @@ function move(event) {
         };
 
     chrId.style.top = (chr.updown()) + "px";
+	
    
 }
 
