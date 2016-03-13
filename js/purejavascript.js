b5ab454d8d01b0747b58b5c1ce6978a2d20fa186
christianpetri <christianpetri@hotmail.com>
2016-03-13 01:12:53 +0100

    //text added

--- js/purejavascript.js
+++ js/purejavascript.js
@@ -14,21 +14,29 @@ function game(){
 	document.getElementById("computer").style.height=100+"px";
 
 	setInterval(function(){
-		
+		//if ball touches upper or lower limit change direction
 		if(ball.x<=0){dir.x=1;}
 		if(ball.x>=290){dir.x=-1;}
+		//if ball touches the left or right boundery, restart game
 		if(ball.y<=0){restartGame=true;}
-		if(ball.y>=690){dir.y=-1;}	
+		if(ball.y>=690){restartGame=true;}	
+		//after restart=true, reset ball to center and give it a random start direction
 		if(restartGame){
 			ball={x:150,y:350};
 			dir.y=(-1 + Math.round(Math.random()) * 2);
 			restartGame=false;
 		}
+		//make ball move
 		ball.x+=10*dir.x;
 		ball.y+=10*dir.y;
-		document.getElementById("computer").style.top=ball.x+"px";
 		document.getElementById("ball").style.top=ball.x+"px";
 		document.getElementById("ball").style.left=ball.y+"px";
+		//make computer move
+		if(ball.y>300){
+		document.getElementById("computer").style.top=ball.x+"px";
+		}
+		
+		//get left and right bars positon to be able to create if statments
 		barLeft.x1=parseInt(document.getElementById("moveupdown").style.top);
 		barLeft.x2=		parseInt(document.getElementById("moveupdown").style.top)+
 						parseInt(document.getElementById("moveupdown").style.height);
@@ -36,10 +44,11 @@ function game(){
 		barRight.x1=parseInt(document.getElementById("computer").style.top);
 		barRight.x2=		parseInt(document.getElementById("computer").style.top)+
 					parseInt(document.getElementById("computer").style.height);
-		
+		//if the ball=my bar position, game on
 		if(barLeft.x1<=ball.x && barLeft.x2>=ball.x&&ball.y===30){
 			dir.y=1;
 		}
+		//if the ball=the computer bar, game on
 		if(barRight.x1<=ball.x && barRight.x2>=ball.x&&ball.y===660){
 			dir.y=-1;
 		}
