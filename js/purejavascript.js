3c7389652606e01d20c4fbc264ed97de0d53a98f
christianpetri <christianpetri@hotmail.com>
2016-03-13 01:02:56 +0100

    Basics are working

--- js/purejavascript.js
+++ js/purejavascript.js
@@ -4,35 +4,49 @@ function game(){
 	"use strict";
 	var ball={x:150,y:350};
 	var dir={x:1,y:1};
-	var bar={x1:1,x2:1};
+	var barLeft={x1:1,x2:1};
+	var barRight={x1:1,x2:1};
 	var restartGame=false;
 
 	document.getElementById("moveupdown").style.top=10+"px";
 	document.getElementById("moveupdown").style.height=100+"px";
+	document.getElementById("computer").style.top=10+"px";
+	document.getElementById("computer").style.height=100+"px";
 
 	setInterval(function(){
 		
-		if(ball.x<=0){restartGame=true;}
+		if(ball.x<=0){dir.x=1;}
 		if(ball.x>=290){dir.x=-1;}
-		if(ball.y<=0){dir.y=1;}
+		if(ball.y<=0){restartGame=true;}
 		if(ball.y>=690){dir.y=-1;}	
+		if(restartGame){
+			ball={x:150,y:350};
+			dir.y=(-1 + Math.round(Math.random()) * 2);
+			restartGame=false;
+		}
 		ball.x+=10*dir.x;
 		ball.y+=10*dir.y;
-		
+		document.getElementById("computer").style.top=ball.x+"px";
 		document.getElementById("ball").style.top=ball.x+"px";
 		document.getElementById("ball").style.left=ball.y+"px";
-		bar.x1=parseInt(document.getElementById("moveupdown").style.top);
-		bar.x2=
-		parseInt(document.getElementById("moveupdown").style.top)+
-		parseInt(document.getElementById("moveupdown").style.height);
+		barLeft.x1=parseInt(document.getElementById("moveupdown").style.top);
+		barLeft.x2=		parseInt(document.getElementById("moveupdown").style.top)+
+						parseInt(document.getElementById("moveupdown").style.height);
+					
+		barRight.x1=parseInt(document.getElementById("computer").style.top);
+		barRight.x2=		parseInt(document.getElementById("computer").style.top)+
+					parseInt(document.getElementById("computer").style.height);
 		
-		if(bar.x1<=ball.x && bar.x2>=ball.x&&ball.y===30){
+		if(barLeft.x1<=ball.x && barLeft.x2>=ball.x&&ball.y===30){
 			dir.y=1;
 		}
+		if(barRight.x1<=ball.x && barRight.x2>=ball.x&&ball.y===660){
+			dir.y=-1;
+		}
 		//parseInt(a, 10)
-		console.log(bar);
+		console.log(barRight);
 		console.log(ball);
-		if(restartGame){ball={x:150,y:350};}
+		
 		
 	},150);
 
