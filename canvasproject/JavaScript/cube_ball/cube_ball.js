76e354b6633ada91737d6d3b3039fd5b2ace0603
christianpetri <christianpetri@hotmail.com>
2016-03-24 16:53:36 +0100

    going home

--- canvasproject/JavaScript/cube_ball/cube_ball.js
+++ canvasproject/JavaScript/cube_ball/cube_ball.js
@@ -51,7 +51,7 @@ $(document).ready(function(){
 			var cube1 = new THREE.Mesh( geometry, material );
 			scene.add( cube1 );
 
-			//add lines
+			//add line
 			material = new THREE.LineBasicMaterial({
 					color: "rgb(255,255,255)"
 				});
@@ -64,6 +64,34 @@ $(document).ready(function(){
 
 			var line = new THREE.Line( geometry, material );
 			scene.add( line ); 
+			
+			//add line1
+			material = new THREE.LineBasicMaterial({
+					color: "rgb(255,255,255)"
+				});
+				
+				geometry = new THREE.Geometry();
+				geometry.vertices.push(					  
+					new THREE.Vector3( 0, 0, 0 ),
+					new THREE.Vector3( 0, 0, 0 )
+				);
+
+			var line1 = new THREE.Line( geometry, material );
+			scene.add( line1 );
+			
+			//add line1
+			material = new THREE.LineBasicMaterial({
+					color: "rgb(255,255,255)"
+				});
+				
+				geometry = new THREE.Geometry();
+				geometry.vertices.push(					  
+					new THREE.Vector3( 0, 0, 0 ),
+					new THREE.Vector3( 0, 0, 0 )
+				);
+
+			var line2 = new THREE.Line( geometry, material );
+			scene.add( line2 );
 
 			var render = function () {
 				requestAnimationFrame( render );
@@ -89,14 +117,33 @@ $(document).ready(function(){
 				line.geometry.vertices[ 1 ].x =mouseX/ratioX;  
 				line.geometry.vertices[ 1 ].y =mouseY/ratioY;  
 				line.geometry.vertices[ 1 ].z =49;
+				
 				 //mouseVector = new THREE.Vector3( mouseX/10, mouseY/10, 0 );
 				//cube1.position.x=mouseX/10;
 				//cube1.position.y=mouseY/10;
 				cube1.position.z=-49;
 				
-				cube.rotation.y =mouseX/1000;
+				cube.rotation.y = mouseX/1000;
 				cube.rotation.x= (mouseY*-1/1000) ;
+				
 				camera.position.z=50;
+				
+				
+				var lineA = line.geometry.vertices[ 1 ];
+				var b = new THREE.Vector3(0,1,0);
+
+				var c = new THREE.Vector3();
+				c.crossVectors( lineA, b );
+				
+				line1.geometry.verticesNeedUpdate = true;
+				line2.geometry.verticesNeedUpdate = true;
+				line1.geometry.vertices[ 1 ]=c;
+				
+				var crossLine1Line2 = new THREE.Vector3();
+				crossLine1Line2.crossVectors(lineA,c);
+				line2.geometry.vertices[ 1 ]=crossLine1Line2;
+				
+				
 				renderer.render(scene, camera);
 				
 				
@@ -104,7 +151,10 @@ $(document).ready(function(){
 			  
 			
     	
-			$(".mouseCoordinates").html("<p> mouseX: "+mouseX+" "+" mouseY: "+mouseY*-1+"width= "+width.toFixed(2)+" camera.fov= "+camera.fov+"</p>");
+			$(".mouseCoordinates").html("<p> mouseX: "+mouseX+" "+" mouseY: "+mouseY*-1+"width= "+
+				width.toFixed(2)+" camera.fov= "+camera.fov+"   x="+c.x+" y="+c.y+" z="+c.z.toFixed(2) +" "+
+				"line.geometry.vertices[ 1 ].x "+line.geometry.vertices[ 1 ].x.toFixed(2)
+				+"</p>");
 			};
 
 			render();
